vivid-planet/comet

Rework content scopes management in the user permissions panel
Render a consistent set of content scope columns for every user based on the
content scope dimensions declared in the API, showing a wildcard dimension as
"All". Assign content scopes through an add dialog with cascading dropdowns
per enumerable dimension and free text values for non-enumerable dimensions.
Use the same paginated grid and add dialog for the assigned and the
permission-specific content scopes, show manually assigned scopes first, and
confirm scope removal. Show the distinct permission count and the accessible
content scope count in the users list.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01JVwZJtKmfrQq2VZmPAB76j

packages/admin/cms-admin/src/userPermissions/user/permissions/AddContentScopeDialog.tsx
@@ -1,0 +1,43 @@
+import { CancelButton, messages, SaveBoundary, SaveBoundarySaveButton } from "@comet/admin";
+import { Add } from "@comet/admin-icons";
+import {
+    // eslint-disable-next-line no-restricted-imports
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+} from "@mui/material";
+import { FormattedMessage } from "react-intl";
+
+import type { ContentScope } from "./ContentScopeDataGrid";
+import { SelectScopesDialogContent } from "./selectScopesDialogContent/SelectScopesDialogContent";
+
+interface AddContentScopeDialogProps {
+    open: boolean;
+    onClose: () => void;
+    /** Called with the scope selected in the dialog. The dialog closes after this resolves. */
+    onAdd: (scope: ContentScope) => Promise<void> | void;
+}
+
+export function AddContentScopeDialog({ open, onClose, onAdd }: AddContentScopeDialogProps) {
+    return (
+        <SaveBoundary onAfterSave={onClose}>
+            <Dialog open={open} maxWidth="sm" fullWidth>
+                <DialogTitle>
+                    <FormattedMessage id="comet.userPermissions.addScope" defaultMessage="Add scope" />
+                </DialogTitle>
+                <DialogContent>
+                    <SelectScopesDialogContent onSubmit={onAdd} />
+                </DialogContent>
+                <DialogActions>
+                    <CancelButton onClick={onClose}>
+                        <FormattedMessage {...messages.close} />
+                    </CancelButton>
+                    <SaveBoundarySaveButton startIcon={<Add />}>
+                        <FormattedMessage id="comet.userPermissions.addScope" defaultMessage="Add scope" />
+                    </SaveBoundarySaveButton>
+                </DialogActions>
+            </Dialog>
+        </SaveBoundary>
+    );
+}

packages/admin/cms-admin/src/userPermissions/user/permissions/PermissionGrid.tsx
@@ -1,5 +1,5 @@
-import { gql, useQuery } from "@apollo/client";
-import { Button, DataGridToolbar, FieldSet, FillSpace, GridCellContent, type GridColDef, TableDeleteButton } from "@comet/admin";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { Button, DataGridToolbar, DeleteDialog, FieldSet, FillSpace, GridCellContent, type GridColDef } from "@comet/admin";
 import { Add, Delete, Edit, StateFilled } from "@comet/admin-icons";
 import { IconButton, Typography } from "@mui/material";
 import type { GridToolbarProps } from "@mui/x-data-grid";
@@ -12,6 +12,8 @@ import { camelCaseToHumanReadable } from "../../utils/camelCaseToHumanReadable";
 import { OverrideContentScopesDialog } from "./OverrideContentScopesDialog";
 import { PermissionDialog } from "./PermissionDialog";
 import {
+    type GQLDeletePermissionMutation,
+    type GQLDeletePermissionMutationVariables,
     type GQLPermissionForGridFragment,
     type GQLPermissionsQuery,
     type GQLPermissionsQueryVariables,
@@ -35,6 +37,13 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
     const intl = useIntl();
     const [permissionId, setPermissionId] = useState<string | "add" | null>(null);
     const [overrideContentScopesId, setOverrideContentScopesId] = useState<string | null>(null);
+    const [permissionToDelete, setPermissionToDelete] = useState<string | null>(null);
+
+    const [deletePermission] = useMutation<GQLDeletePermissionMutation, GQLDeletePermissionMutationVariables>(gql`
+        mutation DeletePermission($id: ID!) {
+            userPermissionsDeletePermission(id: $id)
+        }
+    `);
 
     const { data, loading, error } = useQuery<GQLPermissionsQuery, GQLPermissionsQueryVariables>(
         gql`
@@ -101,12 +110,6 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
             ),
         },
         {
-            field: "source",
-            width: 200,
-            pinnable: false,
-            headerName: intl.formatMessage({ id: "comet.userPermissions.source", defaultMessage: "Assignment type" }),
-        },
-        {
             field: "validityPeriod",
             width: 200,
             pinnable: false,
@@ -131,6 +134,18 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
                 ),
         },
         {
+            field: "source",
+            width: 200,
+            pinnable: false,
+            headerName: intl.formatMessage({ id: "comet.userPermissions.source", defaultMessage: "Assignment type" }),
+            renderCell: ({ row }) =>
+                row.source === "BY_RULE" ? (
+                    <FormattedMessage id="comet.userPermissions.assignmentType.byRule" defaultMessage="By rule" />
+                ) : (
+                    <FormattedMessage id="comet.userPermissions.assignmentType.manual" defaultMessage="Manual" />
+                ),
+        },
+        {
             field: "actions",
             type: "actions",
             headerName: "",
@@ -152,17 +167,9 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
                         </IconButton>
 
                         {row.source !== "BY_RULE" && (
-                            <TableDeleteButton
-                                icon={<Delete />}
-                                mutation={gql`
-                                    mutation DeletePermission($id: ID!) {
-                                        userPermissionsDeletePermission(id: $id)
-                                    }
-                                `}
-                                selectedId={`${row.id}`}
-                                text=""
-                                refetchQueries={[namedOperations.Query.Permissions]}
-                            />
+                            <IconButton onClick={() => setPermissionToDelete(row.id)}>
+                                <Delete />
+                            </IconButton>
                         )}
                     </>
                 );
@@ -213,6 +220,21 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
                 />
             )}
             {permissionId && <PermissionDialog userId={userId} permissionId={permissionId} handleDialogClose={() => setPermissionId(null)} />}
+            <DeleteDialog
+                dialogOpen={permissionToDelete !== null}
+                deleteType="remove"
+                onCancel={() => setPermissionToDelete(null)}
+                onDelete={async () => {
+                    if (permissionToDelete !== null) {
+                        await deletePermission({
+                            variables: { id: permissionToDelete },
+                            refetchQueries: [namedOperations.Query.Permissions],
+                            awaitRefetchQueries: true,
+                        });
+                    }
+                    setPermissionToDelete(null);
+                }}
+            />
         </FieldSet>
     );
 };

vivid-planet/dextinity

packages/admin/cms-admin/src/userPermissions/user/permissions/AddContentScopeDialog.tsx
@@ -1,0 +1,43 @@
+import { CancelButton, messages, SaveBoundary, SaveBoundarySaveButton } from "@dextinity/admin";
+import { Add } from "@dextinity/admin-icons";
+import {
+    // eslint-disable-next-line no-restricted-imports
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+} from "@mui/material";
+import { FormattedMessage } from "react-intl";
+
+import type { ContentScope } from "./ContentScopeDataGrid";
+import { SelectScopesDialogContent } from "./selectScopesDialogContent/SelectScopesDialogContent";
+
+interface AddContentScopeDialogProps {
+    open: boolean;
+    onClose: () => void;
+    /** Called with the scope selected in the dialog. The dialog closes after this resolves. */
+    onAdd: (scope: ContentScope) => Promise<void> | void;
+}
+
+export function AddContentScopeDialog({ open, onClose, onAdd }: AddContentScopeDialogProps) {
+    return (
+        <SaveBoundary onAfterSave={onClose}>
+            <Dialog open={open} maxWidth="sm" fullWidth>
+                <DialogTitle>
+                    <FormattedMessage id="dextinity.userPermissions.addScope" defaultMessage="Add scope" />
+                </DialogTitle>
+                <DialogContent>
+                    <SelectScopesDialogContent onSubmit={onAdd} />
+                </DialogContent>
+                <DialogActions>
+                    <CancelButton onClick={onClose}>
+                        <FormattedMessage {...messages.close} />
+                    </CancelButton>
+                    <SaveBoundarySaveButton startIcon={<Add />}>
+                        <FormattedMessage id="dextinity.userPermissions.addScope" defaultMessage="Add scope" />
+                    </SaveBoundarySaveButton>
+                </DialogActions>
+            </Dialog>
+        </SaveBoundary>
+    );
+}

packages/admin/cms-admin/src/userPermissions/user/permissions/PermissionGrid.tsx
@@ -1,5 +1,5 @@
-import { gql, useQuery } from "@apollo/client";
-import { Button, DataGridToolbar, FieldSet, FillSpace, GridCellContent, type GridColDef, TableDeleteButton } from "@dextinity/admin";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { Button, DataGridToolbar, DeleteDialog, FieldSet, FillSpace, GridCellContent, type GridColDef } from "@dextinity/admin";
 import { Add, Delete, Edit, StateFilled } from "@dextinity/admin-icons";
 import { IconButton, Typography } from "@mui/material";
 import type { GridToolbarProps } from "@mui/x-data-grid";
@@ -12,6 +12,8 @@ import { camelCaseToHumanReadable } from "../../utils/camelCaseToHumanReadable";
 import { OverrideContentScopesDialog } from "./OverrideContentScopesDialog";
 import { PermissionDialog } from "./PermissionDialog";
 import {
+    type GQLDeletePermissionMutation,
+    type GQLDeletePermissionMutationVariables,
     type GQLPermissionForGridFragment,
     type GQLPermissionsQuery,
     type GQLPermissionsQueryVariables,
@@ -35,6 +37,13 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
     const intl = useIntl();
     const [permissionId, setPermissionId] = useState<string | "add" | null>(null);
     const [overrideContentScopesId, setOverrideContentScopesId] = useState<string | null>(null);
+    const [permissionToDelete, setPermissionToDelete] = useState<string | null>(null);
+
+    const [deletePermission] = useMutation<GQLDeletePermissionMutation, GQLDeletePermissionMutationVariables>(gql`
+        mutation DeletePermission($id: ID!) {
+            userPermissionsDeletePermission(id: $id)
+        }
+    `);
 
     const { data, loading, error } = useQuery<GQLPermissionsQuery, GQLPermissionsQueryVariables>(
         gql`
@@ -101,12 +110,6 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
             ),
         },
         {
-            field: "source",
-            width: 200,
-            pinnable: false,
-            headerName: intl.formatMessage({ id: "dextinity.userPermissions.source", defaultMessage: "Assignment type" }),
-        },
-        {
             field: "validityPeriod",
             width: 200,
             pinnable: false,
@@ -131,6 +134,18 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
                 ),
         },
         {
+            field: "source",
+            width: 200,
+            pinnable: false,
+            headerName: intl.formatMessage({ id: "dextinity.userPermissions.source", defaultMessage: "Assignment type" }),
+            renderCell: ({ row }) =>
+                row.source === "BY_RULE" ? (
+                    <FormattedMessage id="dextinity.userPermissions.assignmentType.byRule" defaultMessage="By rule" />
+                ) : (
+                    <FormattedMessage id="dextinity.userPermissions.assignmentType.manual" defaultMessage="Manual" />
+                ),
+        },
+        {
             field: "actions",
             type: "actions",
             headerName: "",
@@ -152,17 +167,9 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
                         </IconButton>
 
                         {row.source !== "BY_RULE" && (
-                            <TableDeleteButton
-                                icon={<Delete />}
-                                mutation={gql`
-                                    mutation DeletePermission($id: ID!) {
-                                        userPermissionsDeletePermission(id: $id)
-                                    }
-                                `}
-                                selectedId={`${row.id}`}
-                                text=""
-                                refetchQueries={[namedOperations.Query.Permissions]}
-                            />
+                            <IconButton onClick={() => setPermissionToDelete(row.id)}>
+                                <Delete />
+                            </IconButton>
                         )}
                     </>
                 );
@@ -213,6 +220,21 @@ export const PermissionGrid = ({ userId }: { userId: string }) => {
                 />
             )}
             {permissionId && <PermissionDialog userId={userId} permissionId={permissionId} handleDialogClose={() => setPermissionId(null)} />}
+            <DeleteDialog
+                dialogOpen={permissionToDelete !== null}
+                deleteType="remove"
+                onCancel={() => setPermissionToDelete(null)}
+                onDelete={async () => {
+                    if (permissionToDelete !== null) {
+                        await deletePermission({
+                            variables: { id: permissionToDelete },
+                            refetchQueries: [namedOperations.Query.Permissions],
+                            awaitRefetchQueries: true,
+                        });
+                    }
+                    setPermissionToDelete(null);
+                }}
+            />
         </FieldSet>
     );
 };
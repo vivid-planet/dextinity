vivid-planet/comet

Rework content scopes management in the user permissions panel
Render a consistent set of content scope columns for every user based on the
content scope dimensions declared in the API, showing a wildcard dimension as
"All". Assign content scopes through an add dialog with cascading dropdowns
per enumerable dimension and free text values for non-enumerable dimensions.
Use the same paginated grid and add dialog for the assigned and the
permission-specific content scopes, show manually assigned scopes first, and
confirm scope removal. Show the distinct permission count and the accessible
content scope count in the users list.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01JVwZJtKmfrQq2VZmPAB76j

packages/admin/cms-admin/src/userPermissions/user/permissions/AddContentScopeDialog.tsx
@@ -1,0 +1,43 @@
+import { CancelButton, messages, SaveBoundary, SaveBoundarySaveButton } from "@comet/admin";
+import { Add } from "@comet/admin-icons";
+import {
+    // eslint-disable-next-line no-restricted-imports
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+} from "@mui/material";
+import { FormattedMessage } from "react-intl";
+
+import type { ContentScope } from "./ContentScopeDataGrid";
+import { SelectScopesDialogContent } from "./selectScopesDialogContent/SelectScopesDialogContent";
+
+interface AddContentScopeDialogProps {
+    open: boolean;
+    onClose: () => void;
+    /** Called with the scope selected in the dialog. The dialog closes after this resolves. */
+    onAdd: (scope: ContentScope) => Promise<void> | void;
+}
+
+export function AddContentScopeDialog({ open, onClose, onAdd }: AddContentScopeDialogProps) {
+    return (
+        <SaveBoundary onAfterSave={onClose}>
+            <Dialog open={open} maxWidth="sm" fullWidth>
+                <DialogTitle>
+                    <FormattedMessage id="comet.userPermissions.addScope" defaultMessage="Add scope" />
+                </DialogTitle>
+                <DialogContent>
+                    <SelectScopesDialogContent onSubmit={onAdd} />
+                </DialogContent>
+                <DialogActions>
+                    <CancelButton onClick={onClose}>
+                        <FormattedMessage {...messages.close} />
+                    </CancelButton>
+                    <SaveBoundarySaveButton startIcon={<Add />}>
+                        <FormattedMessage id="comet.userPermissions.addScope" defaultMessage="Add scope" />
+                    </SaveBoundarySaveButton>
+                </DialogActions>
+            </Dialog>
+        </SaveBoundary>
+    );
+}

packages/admin/cms-admin/src/userPermissions/user/permissions/ContentScopeGrid.tsx
@@ -1,55 +1,30 @@
-import { gql, useQuery } from "@apollo/client";
-import {
-    Button,
-    CancelButton,
-    DataGridToolbar,
-    FieldSet,
-    FillSpace,
-    type GridColDef,
-    Loading,
-    messages,
-    SaveBoundary,
-    SaveBoundarySaveButton,
-} from "@comet/admin";
-import { Select } from "@comet/admin-icons";
-import {
-    // eslint-disable-next-line no-restricted-imports
-    Dialog,
-    DialogActions,
-    DialogTitle,
-    Typography,
-} from "@mui/material";
-import type { GridToolbarProps } from "@mui/x-data-grid";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { Button, DeleteDialog, FieldSet, type GridColDef, Loading } from "@comet/admin";
+import { Add, Delete } from "@comet/admin-icons";
+import { IconButton } from "@mui/material";
 import isEqual from "lodash.isequal";
-import { type ReactNode, useState } from "react";
+import { useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
-import { DataGrid } from "../../../dataGrid/DataGrid";
-import { camelCaseToHumanReadable } from "../../utils/camelCaseToHumanReadable";
-import type { GQLContentScopesQuery, GQLContentScopesQueryVariables } from "./ContentScopeGrid.generated";
-import { SelectScopesDialogContent } from "./selectScopesDialogContent/SelectScopesDialogContent";
-import type { GQLAvailableContentScopesQuery } from "./selectScopesDialogContent/SelectScopesDialogContent.generated";
-
-type ContentScope = {
-    [key: string]: string;
-};
-
-interface ToolbarProps extends GridToolbarProps {
-    toolbarAction?: ReactNode;
-}
-
-function ContentScopeGridToolbar({ toolbarAction }: ToolbarProps) {
-    return (
-        <DataGridToolbar>
-            <FillSpace />
-            {toolbarAction}
-        </DataGridToolbar>
-    );
-}
+import { AddContentScopeDialog } from "./AddContentScopeDialog";
+import { type ContentScope, ContentScopeDataGrid } from "./ContentScopeDataGrid";
+import type {
+    GQLContentScopesQuery,
+    GQLContentScopesQueryVariables,
+    GQLUpdateContentScopesMutation,
+    GQLUpdateContentScopesMutationVariables,
+} from "./ContentScopeGrid.generated";
 
 export const ContentScopeGrid = ({ userId }: { userId: string }) => {
     const intl = useIntl();
     const [open, setOpen] = useState(false);
+    const [scopeToDelete, setScopeToDelete] = useState<ContentScope | null>(null);
+
+    const [updateContentScopes] = useMutation<GQLUpdateContentScopesMutation, GQLUpdateContentScopesMutationVariables>(gql`
+        mutation UpdateContentScopes($userId: String!, $input: UserContentScopesInput!) {
+            userPermissionsUpdateContentScopes(userId: $userId, input: $input)
+        }
+    `);
 
     const { data, error } = useQuery<GQLContentScopesQuery, GQLContentScopesQueryVariables>(
         gql`
@@ -58,6 +33,10 @@ export const ContentScopeGrid = ({ userId }: { userId: string }) => {
                 userContentScopesSkipManual: userPermissionsContentScopes(userId: $userId, skipManual: true)
                 availableContentScopes: userPermissionsAvailableContentScopes {
                     scope
+                    label
+                }
+                availableContentScopeDimensions: userPermissionsAvailableContentScopeDimensions {
+                    name
                     label
                 }
             }
@@ -75,78 +54,99 @@ export const ContentScopeGrid = ({ userId }: { userId: string }) => {
         return <Loading />;
     }
 
-    const columns: GridColDef<ContentScope>[] = generateGridColumnsFromContentScopeProperties(data.availableContentScopes);
+    // A user has all content scopes (e.g. an admin) when the rule-based scopes cover every available content scope. Such a
+    // user also has all values for dimensions that are not part of the available content scopes (e.g. an optional dimension).
+    const hasAllContentScopes =
+        data.availableContentScopes.length > 0 &&
+        data.availableContentScopes.every((availableContentScope) =>
+            data.userContentScopesSkipManual.some((scope) => isEqual(scope, availableContentScope.scope)),
+        );
 
-    const toolbarSlotProps: ToolbarProps = {
-        toolbarAction: (
-            <Button startIcon={<Select />} onClick={() => setOpen(true)} variant="primary">
-                <FormattedMessage id="comet.userPermissions.selectScopes" defaultMessage="Assign scopes" />
-            </Button>
-        ),
+    // A scope is rule-based (as opposed to manually assigned) when it is part of the scopes granted by rule. Only manually
+    // assigned scopes can be deleted here.
+    const isRuleBasedScope = (scope: ContentScope) => data.userContentScopesSkipManual.some((ruleBasedScope) => isEqual(ruleBasedScope, scope));
+
+    // Show manually assigned scopes before rule-based ones (sort is stable, so the order within each group is preserved).
+    const sortedContentScopes = [...data.userContentScopes].sort((a, b) => Number(isRuleBasedScope(a)) - Number(isRuleBasedScope(b)));
+
+    const manualContentScopes = data.userContentScopes.filter((contentScope) => !isRuleBasedScope(contentScope));
+
+    const setManualContentScopes = async (contentScopes: ContentScope[]) => {
+        await updateContentScopes({
+            variables: { userId, input: { contentScopes } },
+            refetchQueries: ["ContentScopes"],
+            awaitRefetchQueries: true,
+        });
     };
+
+    const handleAddScope = async (scope: ContentScope) => {
+        if (!manualContentScopes.some((contentScope) => isEqual(contentScope, scope))) {
+            await setManualContentScopes([...manualContentScopes, scope]);
+        }
+    };
+
+    const handleDeleteScope = async (scope: ContentScope) => {
+        await setManualContentScopes(manualContentScopes.filter((contentScope) => !isEqual(contentScope, scope)));
+    };
+
+    const additionalColumns: GridColDef<ContentScope>[] = [
+        {
+            field: "source",
+            width: 200,
+            pinnable: false,
+            sortable: false,
+            filterable: false,
+            headerName: intl.formatMessage({ id: "comet.userPermissions.source", defaultMessage: "Assignment type" }),
+            renderCell: ({ row }) =>
+                isRuleBasedScope(row) ? (
+                    <FormattedMessage id="comet.userPermissions.assignmentType.byRule" defaultMessage="By rule" />
+                ) : (
+                    <FormattedMessage id="comet.userPermissions.assignmentType.manual" defaultMessage="Manual" />
+                ),
+        },
+        {
+            field: "actions",
+            headerName: "",
+            width: 52,
+            align: "right",
+            pinnable: false,
+            sortable: false,
+            filterable: false,
+            renderCell: ({ row }) =>
+                isRuleBasedScope(row) ? null : (
+                    <IconButton onClick={() => setScopeToDelete(row)}>
+                        <Delete />
+                    </IconButton>
+                ),
+        },
+    ];
 
     return (
         <FieldSet title={intl.formatMessage({ id: "comet.userPermissions.assignedScopes", defaultMessage: "Assigned Scopes" })} disablePadding>
-            <DataGrid
-                rows={data.userContentScopes}
-                columns={columns}
-                rowCount={data?.userContentScopes.length ?? 0}
-                loading={false}
-                getRowId={(row) => JSON.stringify(row)}
-                slots={{
-                    toolbar: ContentScopeGridToolbar,
-                }}
-                slotProps={{
-                    toolbar: toolbarSlotProps,
-                }}
-                showToolbar
+            <ContentScopeDataGrid
+                rows={sortedContentScopes}
+                availableContentScopes={data.availableContentScopes}
+                availableContentScopeDimensions={data.availableContentScopeDimensions}
+                hasAllContentScopes={hasAllContentScopes}
+                additionalColumns={additionalColumns}
+                toolbarAction={
+                    <Button startIcon={<Add />} onClick={() => setOpen(true)} variant="primary">
+                        <FormattedMessage id="comet.userPermissions.addScope" defaultMessage="Add scope" />
+                    </Button>
+                }
             />
-            <SaveBoundary
-                onAfterSave={() => {
-                    setOpen(false);
+            <AddContentScopeDialog open={open} onClose={() => setOpen(false)} onAdd={handleAddScope} />
+            <DeleteDialog
+                dialogOpen={scopeToDelete !== null}
+                deleteType="remove"
+                onCancel={() => setScopeToDelete(null)}
+                onDelete={async () => {
+                    if (scopeToDelete !== null) {
+                        await handleDeleteScope(scopeToDelete);
+                    }
+                    setScopeToDelete(null);
                 }}
-            >
-                <Dialog open={open} maxWidth="lg">
-                    <DialogTitle>
-                        <FormattedMessage id="comet.userScopes.dialog.title" defaultMessage="Select scopes" />
-                    </DialogTitle>
-                    <SelectScopesDialogContent
-                        userId={userId}
-                        userContentScopes={data.userContentScopes}
-                        userContentScopesSkipManual={data.userContentScopesSkipManual}
-                    />
-                    <DialogActions>
-                        <CancelButton onClick={() => setOpen(false)}>
-                            <FormattedMessage {...messages.close} />
-                        </CancelButton>
-                        <SaveBoundarySaveButton />
-                    </DialogActions>
-                </Dialog>
-            </SaveBoundary>
+            />
         </FieldSet>
     );
 };
-
-export function generateGridColumnsFromContentScopeProperties(
-    availableContentScopes: GQLAvailableContentScopesQuery["availableContentScopes"],
-): GridColDef[] {
-    const uniquePropertyNames = Array.from(new Set(availableContentScopes.flatMap((item) => Object.keys(item.scope))));
-    return uniquePropertyNames.map((propertyName, index) => {
-        return {
-            field: propertyName,
-            flex: 1,
-            pinnable: false,
-            sortable: false,
-            filterable: true,
-            headerName: camelCaseToHumanReadable(propertyName),
-            renderCell: ({ row }: { row: ContentScope }) => {
-                const contentScopeWithLabel = availableContentScopes.find((availableContentScope) => isEqual(availableContentScope.scope, row));
-                if (contentScopeWithLabel) {
-                    return <Typography variant={index === 0 ? "subtitle2" : "body2"}>{contentScopeWithLabel.label[propertyName]}</Typography>;
-                } else {
-                    return "-";
-                }
-            },
-        };
-    });
-}

vivid-planet/dextinity

packages/admin/cms-admin/src/userPermissions/user/permissions/AddContentScopeDialog.tsx
@@ -1,0 +1,43 @@
+import { CancelButton, messages, SaveBoundary, SaveBoundarySaveButton } from "@dextinity/admin";
+import { Add } from "@dextinity/admin-icons";
+import {
+    // eslint-disable-next-line no-restricted-imports
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+} from "@mui/material";
+import { FormattedMessage } from "react-intl";
+
+import type { ContentScope } from "../../../contentScope/Provider";
+import { SelectScopesDialogContent } from "./selectScopesDialogContent/SelectScopesDialogContent";
+
+interface AddContentScopeDialogProps {
+    open: boolean;
+    onClose: () => void;
+    /** Called with the scope selected in the dialog. The dialog closes after this resolves. */
+    onAdd: (scope: ContentScope) => Promise<void> | void;
+}
+
+export function AddContentScopeDialog({ open, onClose, onAdd }: AddContentScopeDialogProps) {
+    return (
+        <SaveBoundary onAfterSave={onClose}>
+            <Dialog open={open} maxWidth="sm" fullWidth>
+                <DialogTitle>
+                    <FormattedMessage id="dextinity.userPermissions.addScope" defaultMessage="Add scope" />
+                </DialogTitle>
+                <DialogContent>
+                    <SelectScopesDialogContent onSubmit={onAdd} />
+                </DialogContent>
+                <DialogActions>
+                    <CancelButton onClick={onClose}>
+                        <FormattedMessage {...messages.close} />
+                    </CancelButton>
+                    <SaveBoundarySaveButton startIcon={<Add />}>
+                        <FormattedMessage id="dextinity.userPermissions.addScope" defaultMessage="Add scope" />
+                    </SaveBoundarySaveButton>
+                </DialogActions>
+            </Dialog>
+        </SaveBoundary>
+    );
+}

packages/admin/cms-admin/src/userPermissions/user/permissions/ContentScopeGrid.tsx
@@ -1,56 +1,34 @@
-import { gql, useQuery } from "@apollo/client";
-import {
-    Button,
-    CancelButton,
-    DataGridToolbar,
-    FieldSet,
-    FillSpace,
-    type GridColDef,
-    Loading,
-    messages,
-    SaveBoundary,
-    SaveBoundarySaveButton,
-} from "@dextinity/admin";
-import { Select } from "@dextinity/admin-icons";
-import {
-    // eslint-disable-next-line no-restricted-imports
-    Dialog,
-    DialogActions,
-    DialogTitle,
-    Typography,
-} from "@mui/material";
-import type { GridToolbarProps } from "@mui/x-data-grid";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { Button, DeleteDialog, FieldSet, type GridColDef, Loading } from "@dextinity/admin";
+import { Add, Delete } from "@dextinity/admin-icons";
+import { IconButton } from "@mui/material";
 import isEqual from "lodash.isequal";
-import { type ReactNode, useState } from "react";
+import { useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
-import { DataGrid } from "../../../dataGrid/DataGrid";
-import { camelCaseToHumanReadable } from "../../utils/camelCaseToHumanReadable";
+import type { ContentScope } from "../../../contentScope/Provider";
 import { deduplicateContentScopes } from "../../utils/deduplicateContentScopes";
-import type { GQLContentScopesQuery, GQLContentScopesQueryVariables } from "./ContentScopeGrid.generated";
-import { SelectScopesDialogContent } from "./selectScopesDialogContent/SelectScopesDialogContent";
-import type { GQLAvailableContentScopesQuery } from "./selectScopesDialogContent/SelectScopesDialogContent.generated";
-
-type ContentScope = {
-    [key: string]: string;
-};
-
-interface ToolbarProps extends GridToolbarProps {
-    toolbarAction?: ReactNode;
-}
-
-function ContentScopeGridToolbar({ toolbarAction }: ToolbarProps) {
-    return (
-        <DataGridToolbar>
-            <FillSpace />
-            {toolbarAction}
-        </DataGridToolbar>
-    );
-}
+import { AddContentScopeDialog } from "./AddContentScopeDialog";
+import { ContentScopeDataGrid } from "./ContentScopeDataGrid";
+import type {
+    GQLContentScopesQuery,
+    GQLContentScopesQueryVariables,
+    GQLUpdateContentScopesMutation,
+    GQLUpdateContentScopesMutationVariables,
+} from "./ContentScopeGrid.generated";
 
 export const ContentScopeGrid = ({ userId }: { userId: string }) => {
     const intl = useIntl();
     const [open, setOpen] = useState(false);
+    const [scopeToDelete, setScopeToDelete] = useState<ContentScope | null>(null);
+
+    const [updateContentScopes, { loading: updateInProgress }] = useMutation<GQLUpdateContentScopesMutation, GQLUpdateContentScopesMutationVariables>(
+        gql`
+            mutation UpdateContentScopes($userId: String!, $input: UserContentScopesInput!) {
+                userPermissionsUpdateContentScopes(userId: $userId, input: $input)
+            }
+        `,
+    );
 
     const { data, error } = useQuery<GQLContentScopesQuery, GQLContentScopesQueryVariables>(
         gql`
@@ -59,6 +37,10 @@ export const ContentScopeGrid = ({ userId }: { userId: string }) => {
                 userContentScopesSkipManual: userPermissionsContentScopes(userId: $userId, skipManual: true)
                 availableContentScopes: userPermissionsAvailableContentScopes {
                     scope
+                    label
+                }
+                availableContentScopeDimensions: userPermissionsAvailableContentScopeDimensions {
+                    name
                     label
                 }
             }
@@ -76,79 +58,105 @@ export const ContentScopeGrid = ({ userId }: { userId: string }) => {
         return <Loading />;
     }
 
+    // The user's content scopes are a union of rule-based and manually assigned scopes and can therefore contain duplicates.
     const userContentScopes = deduplicateContentScopes(data.userContentScopes);
-    const columns: GridColDef<ContentScope>[] = generateGridColumnsFromContentScopeProperties(data.availableContentScopes);
 
-    const toolbarSlotProps: ToolbarProps = {
-        toolbarAction: (
-            <Button startIcon={<Select />} onClick={() => setOpen(true)} variant="primary">
-                <FormattedMessage id="dextinity.userPermissions.selectScopes" defaultMessage="Assign scopes" />
-            </Button>
-        ),
+    // A user has all content scopes (e.g. an admin) when the rule-based scopes cover every available content scope. Such a
+    // user also has all values for dimensions that are not part of the available content scopes (e.g. an optional dimension).
+    const hasAllContentScopes =
+        data.availableContentScopes.length > 0 &&
+        data.availableContentScopes.every((availableContentScope) =>
+            data.userContentScopesSkipManual.some((scope) => isEqual(scope, availableContentScope.scope)),
+        );
+
+    // A scope is rule-based (as opposed to manually assigned) when it is part of the scopes granted by rule. Only manually
+    // assigned scopes can be deleted here.
+    const isRuleBasedScope = (scope: ContentScope) => data.userContentScopesSkipManual.some((ruleBasedScope) => isEqual(ruleBasedScope, scope));
+
+    // Show manually assigned scopes before rule-based ones (sort is stable, so the order within each group is preserved).
+    const sortedContentScopes = [...userContentScopes].sort((a, b) => Number(isRuleBasedScope(a)) - Number(isRuleBasedScope(b)));
+
+    const manualContentScopes = userContentScopes.filter((contentScope) => !isRuleBasedScope(contentScope));
+
+    const setManualContentScopes = async (contentScopes: ContentScope[]) => {
+        await updateContentScopes({
+            variables: { userId, input: { contentScopes } },
+            refetchQueries: ["ContentScopes"],
+            awaitRefetchQueries: true,
+        });
     };
+
+    const handleAddScope = async (scope: ContentScope) => {
+        if (!manualContentScopes.some((contentScope) => isEqual(contentScope, scope))) {
+            await setManualContentScopes([...manualContentScopes, scope]);
+        }
+    };
+
+    const handleDeleteScope = async (scope: ContentScope) => {
+        await setManualContentScopes(manualContentScopes.filter((contentScope) => !isEqual(contentScope, scope)));
+    };
+
+    const additionalColumns: GridColDef<ContentScope>[] = [
+        {
+            field: "source",
+            width: 200,
+            pinnable: false,
+            sortable: false,
+            filterable: false,
+            headerName: intl.formatMessage({ id: "dextinity.userPermissions.source", defaultMessage: "Assignment type" }),
+            renderCell: ({ row }) =>
+                isRuleBasedScope(row) ? (
+                    <FormattedMessage id="dextinity.userPermissions.assignmentType.byRule" defaultMessage="By rule" />
+                ) : (
+                    <FormattedMessage id="dextinity.userPermissions.assignmentType.manual" defaultMessage="Manual" />
+                ),
+        },
+        {
+            field: "actions",
+            headerName: "",
+            width: 52,
+            align: "right",
+            pinnable: false,
+            sortable: false,
+            filterable: false,
+            renderCell: ({ row }) =>
+                isRuleBasedScope(row) ? null : (
+                    <IconButton onClick={() => setScopeToDelete(row)} disabled={updateInProgress}>
+                        <Delete />
+                    </IconButton>
+                ),
+        },
+    ];
 
     return (
         <FieldSet title={intl.formatMessage({ id: "dextinity.userPermissions.assignedScopes", defaultMessage: "Assigned Scopes" })} disablePadding>
-            <DataGrid
-                rows={userContentScopes}
-                columns={columns}
-                rowCount={userContentScopes.length}
-                loading={false}
-                getRowId={(row) => JSON.stringify(row)}
-                slots={{
-                    toolbar: ContentScopeGridToolbar,
-                }}
-                slotProps={{
-                    toolbar: toolbarSlotProps,
-                }}
-                showToolbar
+            <ContentScopeDataGrid
+                rows={sortedContentScopes}
+                availableContentScopes={data.availableContentScopes}
+                availableContentScopeDimensions={data.availableContentScopeDimensions}
+                hasAllContentScopes={hasAllContentScopes}
+                additionalColumns={additionalColumns}
+                toolbarAction={
+                    <Button startIcon={<Add />} onClick={() => setOpen(true)} variant="primary" disabled={updateInProgress}>
+                        <FormattedMessage id="dextinity.userPermissions.addScope" defaultMessage="Add scope" />
+                    </Button>
+                }
             />
-            <SaveBoundary
-                onAfterSave={() => {
-                    setOpen(false);
+            <AddContentScopeDialog open={open} onClose={() => setOpen(false)} onAdd={handleAddScope} />
+            <DeleteDialog
+                dialogOpen={scopeToDelete !== null}
+                deleteType="remove"
+                onCancel={() => setScopeToDelete(null)}
+                onDelete={async () => {
+                    try {
+                        if (scopeToDelete !== null) {
+                            await handleDeleteScope(scopeToDelete);
+                        }
+                    } finally {
+                        setScopeToDelete(null);
+                    }
                 }}
-            >
-                <Dialog open={open} maxWidth="lg">
-                    <DialogTitle>
-                        <FormattedMessage id="dextinity.userScopes.dialog.title" defaultMessage="Select scopes" />
-                    </DialogTitle>
-                    <SelectScopesDialogContent
-                        userId={userId}
-                        userContentScopes={userContentScopes}
-                        userContentScopesSkipManual={data.userContentScopesSkipManual}
-                    />
-                    <DialogActions>
-                        <CancelButton onClick={() => setOpen(false)}>
-                            <FormattedMessage {...messages.close} />
-                        </CancelButton>
-                        <SaveBoundarySaveButton />
-                    </DialogActions>
-                </Dialog>
-            </SaveBoundary>
+            />
         </FieldSet>
     );
 };
-
-export function generateGridColumnsFromContentScopeProperties(
-    availableContentScopes: GQLAvailableContentScopesQuery["availableContentScopes"],
-): GridColDef[] {
-    const uniquePropertyNames = Array.from(new Set(availableContentScopes.flatMap((item) => Object.keys(item.scope))));
-    return uniquePropertyNames.map((propertyName, index) => {
-        return {
-            field: propertyName,
-            flex: 1,
-            pinnable: false,
-            sortable: false,
-            filterable: true,
-            headerName: camelCaseToHumanReadable(propertyName),
-            renderCell: ({ row }: { row: ContentScope }) => {
-                const contentScopeWithLabel = availableContentScopes.find((availableContentScope) => isEqual(availableContentScope.scope, row));
-                if (contentScopeWithLabel) {
-                    return <Typography variant={index === 0 ? "subtitle2" : "body2"}>{contentScopeWithLabel.label[propertyName]}</Typography>;
-                } else {
-                    return "-";
-                }
-            },
-        };
-    });
-}